a88d57cdab38e56475d9e3583d4a492557b17bf5
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-08 19:26:09 +0530

    Added action library.

---
+++ actions/action.h
@@ -0,0 +1,22 @@
+/***
+ *       Filename:  action.h
+ *
+ *    Description:  description
+ *
+ *        Version:  0.0.1
+ *        Created:  2017-11-08
+ *       Revision:  none
+ *
+ *         Author:  Dilawar Singh <dilawars@ncbs.res.in>
+ *   Organization:  NCBS Bangalore
+ *
+ *        License:  GNU GPL2
+ */
+
+#ifndef ACTION_H
+
+#define ACTION_H
+
+
+
+#endif /* end of include guard: ACTION_H */
---
+++ actions/linux.cpp
@@ -0,0 +1,16 @@
+/***
+ *       Filename:  linux.cpp
+ *
+ *    Description:  Function on linux.
+ *
+ *        Version:  0.0.1
+ *        Created:  2017-11-08
+ *       Revision:  none
+ *
+ *         Author:  Dilawar Singh <dilawars@ncbs.res.in>
+ *   Organization:  NCBS Bangalore
+ *
+ *        License:  GNU GPL2
+ */
+
+#include "action.h"
