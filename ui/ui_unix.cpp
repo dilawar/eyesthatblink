64b1b4b3169b6d42c4acee5ef94a0251b0be2967
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-14 14:39:53 +0530

    Beatification later.

--- ui/ui_unix.cpp
+++ ui/ui_unix.cpp
@@ -44,6 +44,7 @@ extern ConfigManager config_manager_;
 
 int nana_callback(  nana::picture& canvas )
 {
+    static bool iconfileIsShown = false;
     auto t0 = std::clock( );
     int fixedWidth = 200;
     cv::Mat face;
@@ -53,12 +54,21 @@ int nana_callback(  nana::picture& canvas )
         int width = face.cols;
         cv::resize( face, face
                 , cv::Size( fixedWidth, face.rows * fixedWidth / width ) );
-        
         nana::paint::image img;
         vector<uchar> vec;
         cv::imencode( ".png", face, vec );
         img.open( vec.data( ), vec.size( ) );
         canvas.load( img );
+        iconfileIsShown = false;
+    }
+    else
+    {
+        // Put icon.
+        if( ! iconfileIsShown )
+        {
+            canvas.load( nana::paint::image( ICONFILE_PATH ) );
+            iconfileIsShown = true;
+        }
     }
     auto t1 = clock( );
     time_to_process_one_frame_ = 1000.0 * ( t1 - t0 ) / CLOCKS_PER_SEC;
@@ -87,65 +97,76 @@ void setShowUserFace( bool value )
     config_manager_.setShowUserFace( value );
 }
 
-// Show user face in main window.
-bool show_user_face( const cv::Mat& gray )
+void setBlinkThreshold( int val )
 {
-    //pApp_->show_user_face( gray );
-    return true;
-}
-
-bool show_icon( )
-{
-    //pApp_->show_icon( );
-    return true;
+    config_manager_.setBlinkThreshold( val );
 }
 
 int unix_ui( int argc, char* argv[ ] )
 {
 
-    nana::form fm;
+    nana::form fm( {0,0,400,200} );
     fm.caption( "EyesThatBlink" );
     nana::place layout( fm );
 
     // Layout of gui.
-    layout.div( R"(<vert <horizontal <vert <a><b>> <vert <pic><c>>> 
-        <d weight=20 margin=1>>)" );
+    layout.div( R"(
+        <vert gap=5
+            <vert gap=5
+                <<SmallEye><WearningGlass>> 
+                <<Label weight=20%> <Thres weight=80%> > 
+            > 
+            < <ShowEyes weight=40%> <Canvas weight=60%> >
+        >
+        )" );
+
 
     // Checkbox. Now add click signal.
-    nana::checkbox eye( fm, "I have small eyes" );
+    nana::checkbox eye( fm, "Small eyes" );
     eye.check( config_manager_.getValue<bool>( KEY_USER_HAS_SMALL_EYESS ));
     eye.events( ).click( [&eye]( ) { setUserHasSmallEyes( eye.checked( ) ); } );
 
     // Checkbox. Now add if wearning glasses.
-    nana::checkbox glass( fm, "I am wearning glasses" );
+    nana::checkbox glass( fm, "I wear glasses" );
     glass.check( config_manager_.getValue<bool>( KEY_USER_WEARING_GLASSES ) );
     glass.events( ).click( [&glass]( ) { 
             setUserWearningGlasses( glass.checked( ) ); 
             });
 
-    nana::checkbox showMyFace( fm, "Show my face" );
+    nana::checkbox showMyFace( fm, "Show face" );
     showMyFace.check( config_manager_.getValue<bool>( KEY_SHOW_USER_FACE ) );
     showMyFace.events( ).click( [&showMyFace]( ) { 
             setShowUserFace( showMyFace.checked( ) ); 
             } );
 
-    // Add slider.
-    nana::slider thres(fm );
+    // Add slider and label to it. The offset is 10 which is minimum blink per
+    // minute user is allowed.
+    nana::label dl( fm, "Set notification level: " );
+    nana::slider thres( fm );
+    thres.value( config_manager_.getValue<unsigned int>( KEY_BLINK_RATE_PER_MINUTE ) );
     thres.maximum( 12 );
     thres.move_step( true );
     thres.vernier( [](unsigned maximum, unsigned val ) {
             return std::to_string( 10 + val ) + " blinks per min";
             } );
+    thres.events( ).click( [&thres] {
+            int val = thres.value( ) + 10;
+            setBlinkThreshold( val );
+            });
 
     nana::picture canvas( fm, true );
     canvas.load( nana::paint::image( config_manager_.getIconpath() ));
 
-    layout[ "a" ] << eye;
-    layout[ "b" ] << glass;
-    layout[ "c" ] << showMyFace;
-    layout[ "d" ] << thres;
-     
-    layout[ "pic" ] << canvas;
+
+    layout[ "SmallEye" ] << eye;
+    layout[ "WearningGlass" ] << glass;
+
+    // Add a textbox as label to slider.
+    layout[ "Label" ] << dl;
+    layout[ "Thres" ] << thres;
+
+    layout[ "ShowEyes" ] << showMyFace;
+    layout[ "Canvas" ] << canvas;
     layout.collocate( );
 
     // Add nana timer.
