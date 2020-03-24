efca44ba1197ba78c7fa5f165ae63ea79510fc4d
Dilawar Singh <dilawars@ncbs.res.in>
2020-03-24 23:00:28 +0530

    fixes to python3.

--- test_opencv.py
+++ test_opencv.py
@@ -5,14 +5,11 @@ def main( ):
     if cap.isOpened( ):
         while True:
             ret, frame = cap.read( )
-            print ret
             if ret:
                 cv2.imshow( 'figure', frame )
                 cv2.waitKey( 10 )
     else:
         print( 'Could not open camera' )
-
-
 
 
 if __name__ == '__main__':
