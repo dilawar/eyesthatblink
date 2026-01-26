27459f0571b4e7ed4c6a9d5e371d3738501e53b4
Dilawar Singh <dilawar.s.rajput@gmail.com>
2026-01-26 17:13:12 +0530

    can read frame from camera

---
+++ src/camera.rs
@@ -0,0 +1,52 @@
+use opencv::prelude::*;
+use opencv::videoio;
+
+#[derive(Default)]
+pub(crate) struct Camera {
+    camera: Option<videoio::VideoCapture>,
+}
+
+impl Camera {
+    pub fn new() -> Self {
+        let camera = videoio::VideoCapture::new(0, videoio::CAP_ANY)
+            .inspect_err(|e| tracing::error!("Failed to create VideoCapture: {e}"))
+            .ok();
+
+        Self {
+            camera
+        }
+    }
+
+    pub fn start(&mut self) {
+        if self.camera.is_none() {
+            tracing::error!("No camera found");
+
+            return;
+        }
+
+        let mut camera = self.camera.as_mut().expect("camera already exists");
+        // if ! videoio::VideoCapture::is_opened(&mut camera).expect("failed to check if camera is opened") {
+        //     tracing::error!("Camera could not be opened.");
+        //     return;
+        // }
+
+        tracing::info!("Starting camera");
+        loop {
+            let mut frame = Mat::default();
+            match camera.read(&mut frame) {
+                Ok(true) => {
+                    tracing::info!("Frame read {frame:?}");
+                    std::thread::sleep(std::time::Duration::from_millis(10));
+                }
+                Ok(false) => {
+                    tracing::info!("Frame could not be read.");
+                    std::thread::sleep(std::time::Duration::from_millis(100));
+                }
+                Err(e) => {
+                    tracing::error!("Failed to read frame: {e}");
+                    std::thread::sleep(std::time::Duration::from_millis(1000));
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use tracing_subscriber::{EnvFilter, fmt, prelude::*};
+
+mod camera;
+use camera::Camera;
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+
+    let mut camera = Camera::new();
+    camera.start();
 }
