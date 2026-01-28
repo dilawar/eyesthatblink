59b43530b4134ab17a35343ec810868df73a3116
Dilawar Singh <dilawar.s.rajput@gmail.com>
2026-01-28 22:45:07 +0530

    Adds all cascades

--- src/blink.rs
+++ src/blink.rs
@@ -1,21 +1,42 @@
 use crossbeam_channel::Receiver;
 use opencv::core::Mat;
+use opencv::imgproc;
+use opencv::objdetect;
 
 pub(crate) struct BlinkDetector {
     rx: Receiver<Mat>,
+    face_detector: objdetect::CascadeClassifier,
+    eye_detector: objdetect::CascadeClassifier,
 }
 
 impl BlinkDetector {
     pub fn new(rx: Receiver<Mat>) -> Self {
-        Self { rx }
+        let face_detector =
+            objdetect::CascadeClassifier::new("haarcascade_frontalface_default.xml")
+                .expect("failed to load face detector");
+        let eye_detector = objdetect::CascadeClassifier::new("haarcascade_eye.xml")
+            .expect("failed to load eye detector");
+
+        Self {
+            rx,
+            face_detector,
+            eye_detector,
+        }
     }
 
     pub fn start(&mut self) {
         // analyse frames.
         loop {
             if let Ok(frame) = self.rx.recv() {
-                crate::util::show_frame(&frame).expect("Failed to show frame");
+                self.step(frame).expect("Failed to process frame");
             }
         }
+    }
+
+    fn step(&mut self, frame: Mat) -> anyhow::Result<()> {
+        let mut gray = Mat::default();
+        imgproc::cvt_color(&frame, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
+        crate::util::show_frame(&gray).expect("Failed to show frame");
+        Ok(())
     }
 }
